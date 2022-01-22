1e7dd31741c6115a392aff189cbeac70c2fa23ee
0x6D70 <pfeifenberger.markus2003@gmail.com>
2022-01-22 15:41:06 +0100

    add map files

---
+++ src/main/resources/map/tileset.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="CoronaSimTileSet" tilewidth="10" tileheight="10" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="floor">
+  <image width="10" height="10" source="./brown.png"/>
+ </tile>
+ <tile id="1" type="wall">
+  <image width="10" height="10" source="./black.png"/>
+ </tile>
+ <tile id="2" type="toilet">
+  <image width="10" height="10" source="./white.png"/>
+ </tile>
+ <tile id="4" type="pupil position">
+  <image width="10" height="10" source="./brown.png"/>
+ </tile>
+ <tile id="5" type="teacher pos class">
+  <image width="10" height="10" source="./brown.png"/>
+ </tile>
+ <tile id="6" type="teacher pos chamber">
+  <image width="10" height="10" source="./brown.png"/>
+ </tile>
+ <tile id="7" type="entrance">
+  <image width="10" height="10" source="./brown.png"/>
+ </tile>
+</tileset>
